0131630c2d44af2d11c584a09bdbd5a55bf1d41d
Shawn Long <shawnlong636@gmail.com>
2024-10-10 15:01:05 -0700

    feat: added skeleton for oauth endpoint

--- src/lib/index.ts
+++
@@ -1,8 +0,0 @@
-import { InstallProvider } from '@slack/oauth';
-import { SLACK_CLIENT_ID, SLACK_CLIENT_SECRET } from '$env/static/private';
-
-export const installProvider = new InstallProvider({
-	clientId: SLACK_CLIENT_ID,
-	clientSecret: SLACK_CLIENT_SECRET,
-	stateSecret: 'some-state-secret'
-});
